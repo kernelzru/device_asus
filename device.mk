1c33113e6457c137ea73da5f8b646f1dd0655058
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    X00TD: Build common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab
    Signed-off-by: Navin136 <nkwhitehat@gmail.com>

--- device.mk
+++ device.mk
@@ -240,6 +240,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
