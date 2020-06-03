8cc802e685eef0242a15cc358a63ff3bc6836bc1
Nick C <ctwoon@suicide.today>
2020-06-04 00:33:57 +0300

    X00T: props: enable zygote preforking
    faster app launching, as referenced in https://forum.xda-developers.com/asus-zenfone-max-pro-m1/how-to/android-10-faster-app-launch-enabling-t4108817
    
    Change-Id: I7786380f5156624cd8e2f27a41b6464b53a9c159

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -140,4 +140,5 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     debug.sf.early_phase_offset_ns=1500000 \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    persist.device_config.runtime_native.usap_pool_enabled=true
