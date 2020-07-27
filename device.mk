59e84116f3c2c3512d3dec14208834aa18eedb6d
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-07-27 20:12:34 +0530

    X00TD: Import common configs from LA.UM.8.2.r1-07100-sdm660.0
    Change-Id: I6d0f9e11708ea4ca1faacfaf3de429017c8ab889
    Signed-off-by: Navin136 <nkwhitehat@gmail.com>

--- device.mk
+++ device.mk
@@ -7,9 +7,21 @@
 # Inherit from sdm660-common
 $(call inherit-product, device/asus/sdm660-common/sdm660.mk)
 
+# AID/fs configs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
+
+# IPC router config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
@@ -19,9 +31,18 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Powerhint
 EAS_POWERHINT_VARIANT := sdm636
 
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.device.rc
+
+# QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # Thermal configs
 PRODUCT_COPY_FILES += \
