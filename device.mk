b16bd6bebdb91254df84fd1d979a69b93fb66e4f
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-07-28 12:22:41 +0530

    X00TD: Import wifi configs
    * from UL-ASUS_X01BD-WW-17.2018.2004.424-user
    
    Change-Id: I88d4ab454d2387f3b21f2771fe1b22c97e9c5cc9
    Signed-off-by: Navin136 <nkwhitehat@gmail.com>

--- device.mk
+++ device.mk
@@ -232,5 +232,11 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf \
     X00TDWifiOverlay
 
+# Wifi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/asus/X00TD/X00TD-vendor.mk)
