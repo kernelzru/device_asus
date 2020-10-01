6a22ec7ac9438f4b162e07e6c476a92fa15b87cd
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    X00TD: Remove packages at build time  - Update it to align it to https://gitlab.com/EvoX/vendor_gapps
    Signed-off-by: Navin136 <nkwhitehat@gmail.com>

--- device.mk
+++ device.mk
@@ -402,6 +402,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
