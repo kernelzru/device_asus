924dc08c8a79d3ac4a28d90d0a16897c627859cb
Subinsmani <subins.mani@pixysos.com>
2021-09-19 05:20:07 +0530

    X00TD: Upload Latest Google Camera
    Source https://www.celsoazevedo.com/files/android/google-camera/dev-greatness/f/dl8
    
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #
 # Copyright (C) 2020 The LineageOS Project
 #
@@ -12,3 +13,18 @@ ifeq ($(TARGET_DEVICE),X00TD)
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 endif
+=======
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleCameraGoEdition
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := GoogleCameraGoEdition.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
+LOCAL_OVERRIDES_PACKAGES := SnapdragonCamera Snap Camera2
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
+>>>>>>> 350c3b08 (X00TD: Upload Latest Google Camera)
