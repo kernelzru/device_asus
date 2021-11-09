85545d795c9eeb3065f4e7c30240d5348ce1ec0d
Tiktodz <kuplemarkeple@gmail.com>
2021-11-09 15:47:10 +0000

    X00TD: Welcome Komodo

--- komodo_X00TD.mk
+++ komodo_X00TD.mk
@@ -10,13 +10,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/komodo/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+KOMODO_BUILD_TYPE := UNOFFICIAL
+KOMODO_GAPPS_TYPE := nogapps
+IS_PHONE := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := komodo_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
