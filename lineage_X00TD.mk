7a962854178ab88365df0b55276322c65d3e9934
Tiktodz <kuplemarkeple@gmail.com>
2021-11-11 12:05:34 +0000

    X00TD: Bringup Fork-LineageOS
    Change-Id: Ie3db1aa2523f01d7a8d2d2246833734359776efa

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -11,6 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK := true
+
+# Flos Build Type
+TARGET_FLOS := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
