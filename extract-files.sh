696364d3fbd0f7e667a87e1f06e118dec48ca9ce
Navin136 <nkwhitehat@gmail.com>
2021-11-14 09:12:31 +0530

    X00TD: Update Blobs from UL-ASUS_X00T-WW-17.2017.2012.438-user
    Signed-off-by: Navin136 <nkwhitehat@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -59,52 +59,41 @@ function blob_fixup() {
     product/etc/permissions/qti_fingerprint_interface.xml)
         sed -i 's|/system/framework/|/system/product/framework/|g' "${2}"
         ;;
-
     # Rename to fp service avoid conflicts
     vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service_asus.rc)
         sed -i 's|android.hardware.biometrics.fingerprint@2.1-service|android.hardware.biometrics.fingerprint@2.1-service_asus|g' "${2}"
         ;;
-
     # remove android.hidl.base dependency
     vendor/lib/hw/camera.sdm660.so)
         "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
 	;;
-
     # remove android.hidl.base dependency
-    system/lib64/libfm-hci.so | system/lib64/libwfdnative.so | system/lib/libfm-hci.so | system/lib/libwfdnative.so)
+    system_ext/lib64/libfm-hci.so | system_ext/lib64/libwfdnative.so | system_ext/lib/libfm-hci.so | system_ext/lib/libwfdnative.so)
         "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
 	;;
-
     system_ext/etc/init/dpmd.rc)
         sed -i "s|/system/product/bin/|/system/system_ext/bin/|g" "${2}"
         ;;
-
     system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml | system_ext/etc/permissions/telephonyservice.xml)
         sed -i "s|/system/product/framework/|/system/system_ext/framework/|g" "${2}"
         ;;
-
     system_ext/etc/permissions/qcrilhook.xml)
         sed -i 's|/product/framework/qcrilhook.jar|/system_ext/framework/qcrilhook.jar|g' "${2}"
         ;;
-
     system_ext/lib64/libdpmframework.so)
         "${PATCHELF}" --replace-needed "libcutils.so" "libcutils-v29.so" "${2}"
         "${PATCHELF}" --add-needed "libcutils.so" "${2}"
         ;;
-
     system/lib/libwfdaudioclient.so)
         "${PATCHELF}" --set-soname "libwfdaudioclient.so" "${2}"
          ;;
-
     system/lib/libwfdmediautils.so)
         "${PATCHELF}" --set-soname "libwfdmediautils.so" "${2}"
          ;;
-
     system/lib/libwfdmmsink.so)
         "${PATCHELF}" --add-needed "libwfdaudioclient.so" "${2}"
         "${PATCHELF}" --add-needed "libwfdmediautils.so" "${2}"
          ;;
-
     esac
 }
 
