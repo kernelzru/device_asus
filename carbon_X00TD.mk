54a597b5b3d89535c9651cafac5fc779c8f7db7e
Tiktodz <kuplemarkeple@gmail.com>
2021-11-21 13:12:26 +0000

    X00TD: Bringup Carbon

--- carbon_X00TD.mk
+++ carbon_X00TD.mk
@@ -9,14 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CarbonROM stuff
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+CARBON_BUILDTYPE := UNOFFICIAL
+TARGET_USE_GENERIC_WALLPAPER := true
+CARBON_MAINTAINER := Tiktodz
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := carbon_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
