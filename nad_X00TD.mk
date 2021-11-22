c170d69f1900766a37a2415ce97fb7ac6c53a365
Tiktodz <kuplemarkeple@gmail.com>
2021-11-22 13:58:39 +0000

    X00TD: Update redfin fingerprint to `October 2021`
    * Maybe fix the problematic CTS profile
    We try adapt to: https://github.com/NusantaraProject-ROM/android_vendor_nusantara/commit/bdcc817544d4454c541a55d8edf4bb20c286de76
    
    Change-Id: af0fec58ad1b6d3e32047e62826764feb6c78cce
    Signed-off-by: Tiktodz <kuplemarkeple@gmail.com>

--- nad_X00TD.mk
+++ nad_X00TD.mk
@@ -11,7 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Inherit some common Nusantara stuff
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
-$(call inherit-product-if-exists, packages/apps/NusantaraParts/nadproject.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -34,7 +33,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "asus/WW_X00TD/ASUS_X00T_2:9/QKQ1/17.2017.2012.438-20201203:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
