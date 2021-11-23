PixelExperience-Devices/device_asus_X00TD

X00TD: Use Soong namespace to avoid device build conflicts
Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39
Signed-off-by: Subinsmani <subins.mani@pixysos.com>

BoardConfig.mk
@@ -169,6 +169,9 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
+
 # Treble
 BOARD_VNDK_VERSION := current
 

kernelzru/device_asus

Signed-off-by: Navin136 <nkwhitehat@gmail.com>

BoardConfig.mk
@@ -54,6 +54,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_asus
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
 # Treble
 BOARD_VNDK_VERSION := current
 