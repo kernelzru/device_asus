8920eadeb19cb12fa579b437da432da0af5b11d8
Tiktodz <kuplemarkeple@gmail.com>
2022-01-25 08:45:21 +0000

    X00TD: Bringup `POSP`
    Signed-off-by: Tiktodz <kuplemarkeple@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/potato_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug
+    potato_X00TD-user \
+    potato_X00TD-userdebug \
+    potato_X00TD-eng
 
