7adf3f6824b27ebd36df6947df0a365c0e91b109
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-03-02 16:44:39 +0700

    X00TD: Switch to coral February FP

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -30,7 +30,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220105.002 7961164 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 12 SQ1A.220205.002 8010174 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:12/SQ1A.220205.002/8010174:user/release-keys"
