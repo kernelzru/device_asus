057af2059e2e30e74041c7af0a9ed54dcf2b88ee
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-05-09 11:09:14 +0700

    X00TD: Ship overlay

--- device.mk
+++ device.mk
@@ -13,7 +13,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-evolution
 
 # Powerhint
 EAS_POWERHINT_VARIANT := sdm636
