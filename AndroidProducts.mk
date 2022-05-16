2e592422bb4c30633f430c632488922ffd0a3195
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-05-16 11:48:45 +0700

    X00TD: syberia choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/syberia_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug
+    syberia_X00TD-userdebug
 
