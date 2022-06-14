b6d07e0f26c7f36b78e184c91bcef55d7c88ab5e
T.B.S <kuplemarkeple@gmail.com>
2022-06-14 15:53:47 +0700

    X00T: Make It NgabLu

--- lineage_X00T.mk
+++ lineage_X00T.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+LINEAGE_BUILDTYPE := NGABLU
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
