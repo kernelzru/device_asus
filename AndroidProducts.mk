ceb5066d0b842ce26ea83e341c2f5e9dbe0c7dc3
Tiktodz <ewprjkt@proton.me>
2022-08-08 17:42:44 +0000

    X00TD: Derpfest welcome

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/derp_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug
-
+    derp_X00TD-userdebug \
+    derp_X00TD-user \
+    derp_X00TD-eng
--- derp_X00TD.mk
+++ derp_X00TD.mk
@@ -10,13 +10,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Inherit Derpfest
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_DISABLE_POSTRENDER_CLEANUP := true
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := derp_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -27,7 +34,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1309 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.004.A1 8847371 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "asus/WW_X00TD/ASUS_X00T_2:8.1.0/OPM1/15.2016.1805.318-20180712:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.004.A1/8847371:user/release-keys"
