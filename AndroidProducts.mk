b5eb9121f3d88f3e22512a30fdb5deb1c399f110
SKetU <96108085+SKetU-l@users.noreply.github.com>
2022-09-13 08:20:23 +0530

    Supe

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/corvus_X00TD.mk
+    $(LOCAL_DIR)/superior_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    corvus_X00TD-userdebug
+    superior_X00TD-userdebug
 
