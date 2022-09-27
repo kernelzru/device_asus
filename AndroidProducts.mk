40b17f8787b4b580f0a8e52a0e3b544ef8fe7c51
SKetU <96108085+SKetU-l@users.noreply.github.com>
2022-09-27 09:12:27 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_X00TD.mk
+    $(LOCAL_DIR)/flamingo_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_X00TD-userdebug
+    flamingo_X00TD-userdebug
 
