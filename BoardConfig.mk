fef65b56f97f4e4b989149ec61bb0a662913525e
SKetU-l <debasisj059@gmail.com>
2022-10-17 18:23:11 +0530

    X00TD: initial config

---
+++ BoardConfig.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sdm660-common
+include device/asus/sdm660-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/asus/X00TD
+
+# Assert
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_OTA_ASSERT_DEVICE := ASUS_X00TD,X00TD,X00T
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sdm636
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# Kernel
+TARGET_KERNEL_CONFIG := X00TD_defconfig
+
+# Inherit the proprietary files
+include vendor/asus/X00TD/BoardConfigVendor.mk
