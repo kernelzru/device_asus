a546f8157feea6512e47db9a874ceb0875c16583
kernel <kernel@kernelz.ru>
2022-10-25 22:01:48 +0300

    Fixes and updates 2

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+rm -rf  hardware/qcom-caf/msm8998/audio hardware/qcom-caf/msm8998/media hardware/qcom-caf/msm8998/display
+git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8998_display -b thirteen hardware/qcom-caf/msm8998/display
+git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8998_audio -b thirteen hardware/qcom-caf/msm8998/audio
+git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8998_media -b thirteen hardware/qcom-caf/msm8998/media
+
+git clone https://github.com/LineageOS/android_packages_apps_ExactCalculator.git -b lineage-20.0 device/asus/X00TD/ExactCalculator
